ac9e531a4944b270720f8142597dfdc29fe96d91
bryanmax9 <brillant16.gcv@gmail.com>
2024-03-23 00:53:30 -0700

    Finally finished the space Project🌑🔭🌌

--- app/src/main/java/com/example/marsroverapp/MainActivity.kt
+++ app/src/main/java/com/example/marsroverapp/MainActivity.kt
@@ -5,17 +5,22 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
+
+data class MarsPhoto(val imageUrl: String, val description: String)
 
 class MainActivity : AppCompatActivity() {
 
-    private val photosList = mutableListOf<String>()
+    private val photosList = mutableListOf<MarsPhoto>()
     private var currentPhotoIndex = 0
 
     private lateinit var imageView: ImageView
+    private lateinit var descriptionTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,29 +28,29 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.nextImage)
         imageView = findViewById<ImageView>(R.id.marsPhoto)
+        descriptionTextView = findViewById<TextView>(R.id.imageDescription)
 
         getMarsImageURL()
 
         button.setOnClickListener {
-            // Load the next image on double click
             loadNextImage()
         }
     }
 
     private fun loadNextImage() {
-        // If there are no more photos in the list, fetch new ones
         if (currentPhotoIndex >= photosList.size) {
             getMarsImageURL()
             return
         }
 
-        val imageUrl = photosList[currentPhotoIndex]
+        val marsPhoto = photosList[currentPhotoIndex]
         Glide.with(this)
-            .load(imageUrl)
+            .load(marsPhoto.imageUrl)
             .fitCenter()
             .into(imageView)
 
-        // Increment the index or reset if we've reached the end of the list
+        descriptionTextView.text = marsPhoto.description
+
         currentPhotoIndex = (currentPhotoIndex + 1) % photosList.size
     }
 
@@ -54,19 +59,29 @@ class MainActivity : AppCompatActivity() {
         if (photosList.isNotEmpty()) return
 
         val client = AsyncHttpClient()
-        val apiKey = "dgTkYYl5myow4ueF5hVaEf0t9XFl8ssly5zzaQtI" // Use your API key
+        val apiKey = "dgTkYYl5myow4ueF5hVaEf0t9XFl8ssly5zzaQtI" // Replace with your actual API key
         val url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=$apiKey"
 
         client[url, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                val photosArray = json.jsonObject.getJSONArray("photos")
-                for (i in 0 until photosArray.length()) {
-                    val photoObject = photosArray.getJSONObject(i)
-                    val imgSrc = photoObject.getString("img_src")
-                    photosList.add(imgSrc)
+                Log.d("API Response", json.toString())
+                try {
+                    val photosArray = json.jsonObject.getJSONArray("photos")
+                    for (i in 0 until photosArray.length()) {
+                        val photoObject = photosArray.getJSONObject(i)
+                        if (photoObject.has("img_src")) {
+                            val imgSrc = photoObject.getString("img_src")
+                            val cameraObject = photoObject.getJSONObject("camera")
+                            val description = cameraObject.getString("full_name")
+                            photosList.add(MarsPhoto(imgSrc, description))
+                        }
+                    }
+                    // After fetching, load the first image
+                    loadNextImage()
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                    Log.e("JSON Parsing Error", "Error: " + e.localizedMessage)
                 }
-                // After fetching, load the first image
-                loadNextImage()
             }
 
             override fun onFailure(
@@ -80,3 +95,4 @@ class MainActivity : AppCompatActivity() {
         }]
     }
 }
+
